9730dc995d28a41bdf0459509726b70e6e7287bf
Abdul KARIM <139354118+Abdulgithup@users.noreply.github.com>
2023-09-02 08:32:37 +0500

    chp 1 & 2

---
+++ app.js
@@ -0,0 +1,58 @@
+// Chapter#1 Questions
+
+
+alert ("Welcome to my website")
+
+alert("Error! Please enter a valid password")
+
+alert("Welcome to JS world...\nHappy Coding!")
+
+alert("Welcome to JS Land")
+
+alert("Happy Coding!\n▢ prevent this page from creating addtional dialogs.")
+
+alert("hello i can run js through my brower's console")
+
+console.log("hello i can run js through my brower's console")
+
+
+// Chapter#2 Questions
+
+var username
+
+var myname = ("Rabiyan")
+
+var message = ("Hello World")
+
+alert (message)
+
+var fullname = ("Jhone Doe")
+
+var age = ("15 Years Old")
+
+var www = ("Certified Mobile Application Development")
+
+alert (fullname)
+
+alert (age)
+
+alert (www)
+
+var pizza = ("PIZZA\nPIZZ\n\PIZ\nPI\nP")
+
+alert (pizza)
+
+var email = (" Rabiyanmemon@gmail.com")
+var emaildisc = ("My Email Address is")
+
+alert (emaildisc+email)
+
+
+var book = ("A Smarter Way to learn Javascript")
+var bookdisc = ("I am trying to learn from the book ")
+
+alert (bookdisc+book)
+
+document.write("Yah! I can write HTML content through JAVASCRIPT")
+
+alert ("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
